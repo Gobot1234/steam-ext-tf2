fc73bfe83365ff329a225b40803a5fc598a1b2a8
James <gobot1234yt@gmail.com>
2020-07-22 13:42:51 +0100

    Don't let people overwrite steam.TF2

--- steam/ext/tf2/__init__.py
+++ steam/ext/tf2/__init__.py
@@ -11,6 +11,9 @@ from .state import GCState
 
 class GameCoordinatorClient(steam.Client):
     def __init__(self, loop: Optional[asyncio.AbstractEventLoop] = None, **options):
+        popped = options.pop('game', None)
+        if popped:  # don't let them overwrite the main game
+            options['games'] = [popped]
         super().__init__(loop, game=steam.TF2, **options)
         self._connection = GCState(loop=self.loop, client=self, http=self.http)
 
