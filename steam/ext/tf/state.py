b20f8c6e0407d02433d60bd7b6423c4baf0e28b5
James <gobot1234yt@gmail.com>
2020-09-20 13:01:12 +0100

    Use annotations

--- steam/ext/tf/state.py
+++ steam/ext/tf/state.py
@@ -4,13 +4,14 @@ from __future__ import annotations
 
 import asyncio
 import logging
-from typing import Callable, Dict, TYPE_CHECKING
+from typing import Callable, Coroutine, Optional, TYPE_CHECKING
 
 import steam
-from steam.state import ConnectionState, register as register_emsg, EventParser
+from steam.state import ConnectionState, register as register_emsg
 from steam.protobufs import MsgProto, EMsg
 
 from .enums import Language
+from .protobufs.base_gcmessages import CMsgClientGoodbye, CMsgClientWelcome, CMsgServerGoodbye, CMsgServerWelcome
 
 if TYPE_CHECKING:
     from steam import Client
@@ -18,6 +19,8 @@ if TYPE_CHECKING:
     from steam.protobufs.steammessages_clientserver_2 import CMsgGcClient
 
 log = logging.getLogger(__name__)
+DefaultMsg = MsgProto["CMsgGcClient"]
+EventParser = Callable[["GCState", DefaultMsg], Optional[Coroutine[None, None, None]]]
 
 
 class Registerer:
@@ -27,27 +30,27 @@ class Registerer:
         self.func = func
         self.language = language
 
-    def __set_name__(self, state: "GCState", _):
+    def __set_name__(self, state: "GCState", _) -> None:
         state.gc_parsers[self.language] = self.func
 
 
 def register(language: Language) -> Callable[[EventParser], Registerer]:
-    def decorator(func: EventParser):
+    def decorator(func: EventParser) -> Registerer:
         return Registerer(func, language)
 
     return decorator
 
 
 class GCState(ConnectionState):
-    gc_parsers: Dict[Language, EventParser] = dict()
+    gc_parsers: dict[Language, EventParser] = dict()
 
-    def __init__(
-        self, loop: asyncio.AbstractEventLoop, client: "Client", http: "HTTPClient", **kwargs,
-    ):
+    __slots__ = ('_has_gc_session',)
+
+    def __init__(self, loop: asyncio.AbstractEventLoop, client: Client, http: HTTPClient, **kwargs):
         super().__init__(loop, client, http, **kwargs)
 
     @register_emsg(EMsg.ClientFromGC)
-    async def parse_gc_message(self, msg: MsgProto["CMsgGcClient"]):
+    async def parse_gc_message(self, msg: DefaultMsg):
         if msg.body.appid != steam.TF2:
             return
         try:
@@ -60,16 +63,30 @@ class GCState(ConnectionState):
         try:
             func = self.gc_parsers[language]
         except KeyError:
-            log.debug(f"Ignoring event {repr(msg)}")
+            log.debug(f"Ignoring event {msg!r}")
         else:
             await steam.utils.maybe_coroutine(func, self, msg)
 
+    @register(Language.ClientWelcome)
+    def parse_gc_connect(self, msg: DefaultMsg):
+        proto = CMsgClientWelcome().parse(msg.body.payload)
+        self._has_gc_session = True
+        self.dispatch("gc_connect", proto.version)
+
     @register(Language.ServerWelcome)
-    async def parse_gc_logon(self, msg: MsgProto):
-        self.dispatch("gc_connect")
+    def parse_gc_connect(self, msg: DefaultMsg):
+        proto = CMsgServerWelcome().parse(msg.body.payload)
+        self._has_gc_session = True
+        self.dispatch("gc_connect", proto.active_version)
 
     @register(Language.ServerGoodbye)
-    async def parse_gc_logoff(self, msg: MsgProto):
-        self.dispatch("gc_disconnect")
+    def parse_goodbye(self, msg: DefaultMsg):
+        proto = CMsgClientGoodbye().parse(msg.body.payload)
+        self.dispatch("gc_disconnect", proto.reason)
+
+    @register(Language.ServerGoodbye)
+    def parse_goodbye(self, msg: DefaultMsg):
+        proto = CMsgServerGoodbye().parse(msg.body.payload)
+        self.dispatch("gc_disconnect", proto.reason)
 
     # TODO impl https://github.com/DoctorMcKay/node-tf2/blob/master/handlers.js
